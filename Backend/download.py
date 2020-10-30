ce94e6eed4c584af99d8324927d8049193987a42
abhishekbvs <abhishekbvs>
2020-10-30 10:56:03 +0530

    Update code

--- Backend/download.py
+++
@@ -1,5 +0,0 @@
-from google_drive_downloader import GoogleDriveDownloader as gdd
-
-gdd.download_file_from_google_drive(file_id='0B4qKWcILZ0ddTFJsUjBDSHBCd2M',
-                                    dest_path='./data/'+'0B4qKWcILZ0ddTFJsUjBDSHBCd2M'+'.mov',
-                                    unzip=False)
